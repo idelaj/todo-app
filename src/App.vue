ec876721bdb099ee5482b3a7a60f679268fa9c7e
idelaj <idelaj@yahoo.com>
2024-10-23 23:04:48 +0500

    main app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todo_list = ref([])
+const input_content = ref('')
+const todos_order = computed(() => todo_list.value.sort((a,b) =>{
+	return a.createdAt - b.createdAt
+}))
+
+watch(todo_list, (newVal) => {
+	localStorage.setItem('todo_list', JSON.stringify(newVal))
+}, {
+	deep: true
+})
+
+const addItem = () => {
+	if (input_content.value.trim() === '') {
+		return
+	}
+
+	todo_list.value.push({
+		content: input_content.value,
+		done: false,
+		editable: false,
+		createdAt: new Date().getTime()
+	})
+
+  input_content.value = ''
+}
+
+const deleteItem = (todo) => {
+	todo_list.value = todo_list.value.filter((t) => t !== todo)
+}
+
+onMounted(() => {
+	todo_list.value = JSON.parse(localStorage.getItem('todo_list')) || []
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+	<main class="app">
+		<section class="greeting">
+			<h2 class="title">
+				Hi there!
+			</h2>
+		</section>
+		<section class="create-todo">
+			<h3>It is an easy todo-list down below</h3>
+			<form id="new-todo-form" @submit.prevent="addItem">
+				<h4>What's on your todo list?</h4>
+        <div class="input-wrapper">
+          <input type="text" name="content" id="content" placeholder="I have to do..." v-model="input_content" />
+          <input type="submit" value="Add todo" />
+        </div>
+			</form>
+		</section>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+		<section class="todo-list">
+			<h3>Your todo list</h3>
+			<div class="list" id="todo-list">
+
+				<div v-for="todo in todos_order" :class="`todo-item ${todo.done && 'done'}`">
+					<label>
+						<input type="checkbox" v-model="todo.done" />
+            <span class="bubble personal"></span>
+
+					</label>
+					<div class="todo-content">
+						<input type="text" v-model="todo.content" />
+					</div>
+
+					<div class="actions">
+						<button class="delete" @click="deleteItem(todo)">Delete</button>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+	</main>
+</template>
